71e0b232bf11985d800a6656399481c0a56bb1b5
shreyanshshandilya15 <shreytiwari56@gmail.com>
2024-08-22 16:00:48 +0530

    add port

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.get("/",(req,res)=>{
     res.send("hello world");
 });
 
-const port=process.env.PORT ;
+const port=process.env.PORT || 4000;
 app.listen(port,()=>{
     console.log(`server is running on port ${port} successfully !!`);
 })
